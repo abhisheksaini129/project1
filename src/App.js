953f3b68a37f1338fc00984695cec307c849d9b7
ABHISHEK <abhisheksaini084@gmail.com>
2022-09-07 10:36:17 +0530

    project updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useEffect,useState } from 'react';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+  const[data,setdata]=useState([]);
+  const[dataLimit,setdataLimit]=useState(5);
+  const[pageSkipLimit,setPageSkipLimit]=useState(0);
+  useEffect(() => {
+    axios.get(`https://dummyjson.com/products?skip=${pageSkipLimit*dataLimit}&limit=${dataLimit}`)
+    .then(
+      function(response){
+        setdata(response.data.products)
+      }
+    ).catch(function(error){
+     alert(error.message);
+    }) 
+    window.addEventListener('scroll', handleScroll); 
+    function handleScroll() {
+      if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+      console.log('Fetch more list items!');
+      setdataLimit(dataLimit+5);
+    }
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [dataLimit,pageSkipLimit])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {
+        data.map((item,ind)=>(
+        <div className ='productContainer' key={ind}>
+        <img className ='productImage' src={item.images[0]} alt="itemImage"></img>
+        <h2 className ='productTitle'>{item.title}</h2>
+        <p className ='product-description'>{item.description} </p>
+        </div>
+        )
+        )
+      }  
     </div>
   );
 }
